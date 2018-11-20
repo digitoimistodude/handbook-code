3359f5f3129d8e8ed3db3695ce5edda14502bee3
Roni Laukkarinen <roni@laukkarinen.info>
2018-11-20 11:45:40 +0200

    Typography update, fixes

--- content/themes/handbook/js/src/scripts.js
+++ content/themes/handbook/js/src/scripts.js
@@ -8,9 +8,5 @@
     // Fitvids
     $('.entry-content, .entry-content p, .entry-content iframe, .slide-single-article, .slide-single-article .post').fitVids();
 
-    if (window.innerWidth > 600) {
-      $('.side-nav').css( { 'height': ($('.content-area').outerHeight() + 'px' ) });
-    }
-
   });
 } )( jQuery );
