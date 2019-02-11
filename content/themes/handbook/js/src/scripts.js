f3541337f0d67757a78e260d4b0ab8ba31b49162
Henri Avoketo <henri@dude.fi>
2019-02-11 14:55:01 +0200

    Show git version history toggle button styles

--- content/themes/handbook/js/src/scripts.js
+++ content/themes/handbook/js/src/scripts.js
@@ -2,11 +2,19 @@
  * handbook theme JavaScript.
  */
 
-( function( $ ) {
+ ( function( $ ) {
   $(function() {
 
     // Fitvids
     $('.entry-content, .entry-content p, .entry-content iframe, .slide-single-article, .slide-single-article .post').fitVids();
 
   });
+
+  // Toggle onclick git commit history
+  $(document).ready(function() {
+    $(".git-commit-history-toggle").click(function() {
+      $(".commits").slideToggle("slow");
+    });
+  });
+
 } )( jQuery );
--- content/themes/handbook/page.php
+++ content/themes/handbook/page.php
@@ -23,43 +23,41 @@ get_template_part( 'template-parts/hero', get_post_type() ); ?>
       <?php while ( have_posts() ) {
       	the_post();
 
-				$git_commit_info = handbook_get_git_commit_info( get_the_id() );
-				$git_commit_history = handbook_get_git_commit_history( get_the_id() ); ?>
+        $git_commit_info = handbook_get_git_commit_info( get_the_id() );
+        $git_commit_history = handbook_get_git_commit_history( get_the_id() ); ?>
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				  <header class="entry-header">
-				    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-				  </header><!-- .entry-header -->
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+          <header class="entry-header">
+            <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+          </header><!-- .entry-header -->
 
-				  <div class="entry-content">
-				    <?php the_content();
+          <div class="entry-content">
+            <?php the_content();
 
-				      wp_link_pages( array(
-				        'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'handbook' ),
-				        'after'  => '</div>',
-				      ) );
-				    ?>
-				  </div><!-- .entry-content -->
+            wp_link_pages( array(
+              'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'handbook' ),
+              'after'  => '</div>',
+            ) );
+            ?>
+          </div><!-- .entry-content -->
 
-				  <p><?php edit_post_link(); ?></p>
+          <p><?php edit_post_link(); ?></p>
 
-				  <?php if ( ! empty( $git_commit_info ) ) : ?>
-				  	<p class="modified">Viimeksi muokattu käyttäjän <?php echo $git_commit_info->commit->committer->name ?> toimesta viestillä "<?php echo $git_commit_info->commit->message ?>", <a href="<?php echo $git_commit_info->html_url ?>">katso muutos <?php echo str_split( $git_commit_info->sha, 7 )[0] ?> GitHubissa</a>.</p>
-				  <?php endif;
+          <?php if ( ! empty( $git_commit_info ) ) : ?>
+           <p class="modified">Viimeksi muokattu käyttäjän <?php echo $git_commit_info->commit->committer->name ?> toimesta viestillä "<?php echo $git_commit_info->commit->message ?>", <a href="<?php echo $git_commit_info->html_url ?>">katso muutos <?php echo str_split( $git_commit_info->sha, 7 )[0] ?> GitHubissa</a>. <a class="git-commit-history-toggle">Näytä versiohistoria.</a></p>
+         <?php endif;
 
-				  if ( ! empty( $git_commit_history ) ) : ?>
-				  	<div class="git-commit-history" style="display: none;">
-				  		<h2>Versiohistoria</h2>
+         if ( ! empty( $git_commit_history ) ) : ?>
+          <div class="git-commit-history">
+            <div class="commits">
+             <?php foreach ( $git_commit_history as $git_commit ) : ?>
+              <p class="commit"><span class="date"><?php echo date_i18n( 'j.n.Y H:i:s', strtotime( $git_commit['committer']->date ) ); ?></span> Muokattu käyttäjän <?php echo $git_commit['committer']->name ?> toimesta viestillä "<?php echo $git_commit['message'] ?>", <a href="<?php echo $git_commit['html_url'] ?>">katso muutos <?php echo str_split( $git_commit['sha'], 7 )[0] ?> GitHubissa</a>.</p>
+            <?php endforeach; ?>
+          </div>
+        </div>
+      <?php endif; ?>
 
-				  		<div class="commits">
-				  			<?php foreach ( $git_commit_history as $git_commit ) : ?>
-				  				<p class="commit"><span class="date"><?php echo date_i18n( 'j.n.Y H:i:s', strtotime( $git_commit['committer']->date ) ); ?></span> Muokattu käyttäjän <?php echo $git_commit['committer']->name ?> toimesta viestillä "<?php echo $git_commit['message'] ?>", <a href="<?php echo $git_commit['html_url'] ?>">katso muutos <?php echo str_split( $git_commit['sha'], 7 )[0] ?> GitHubissa</a>.</p>
-				  			<?php endforeach; ?>
-				  		</div>
-				  	</div>
-				  <?php endif; ?>
-
-				</article><!-- #post-## -->
+    </article><!-- #post-## -->
 
 				<?php // If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) {
@@ -70,7 +68,7 @@ get_template_part( 'template-parts/hero', get_post_type() ); ?>
       } ?>
     </div><!-- .container -->
 
-	</main><!-- #main -->
+  </main><!-- #main -->
 </div><!-- #primary -->
 
 <?php wp_footer(); ?>
