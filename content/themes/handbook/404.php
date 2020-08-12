84bb426cd201c340a3c26eecead9291e0b1bc776
Roni Laukkarinen <roni@dude.fi>
2020-08-12 11:19:46 +0300

    404

--- content/themes/handbook/404.php
+++ content/themes/handbook/404.php
@@ -17,12 +17,11 @@ get_template_part( 'template-parts/hero', get_post_type() ); ?>
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'handbook' ); ?></h1>
+					<h1 class="page-title">404</h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'handbook' ); ?></p>
-					<?php get_search_form(); ?>
+					<p>Valitettavasti sivua ei löydy. Ehkä se on poistettu.</p>
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
